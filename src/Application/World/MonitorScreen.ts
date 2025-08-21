e27b5e303070e0f43d0757de1b5e88ae7e146079
nam-techie <namdpse180259@fpt.edu.vn>
2025-08-21 09:37:13 +0700

    Update iframe source and title in MonitorScreen component

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://os.henryheffernan.com/';
+        iframe.src = 'https://portfolio-nam-techie.vercel.app/';
         /**
          * Use dev server is query params are present
          *
@@ -204,7 +204,7 @@ export default class MonitorScreen extends EventEmitter {
         iframe.className = 'jitter';
         iframe.id = 'computer-screen';
         iframe.frameBorder = '0';
-        iframe.title = 'HeffernanOS';
+        iframe.title = 'NamTechieOS';
 
         // Add iframe to container
         container.appendChild(iframe);
